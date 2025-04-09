19d491a60fb6e0439a29d96f1ff02b55bac7141b
abelgtzrs <32408235+abelgtzrs@users.noreply.github.com>
2025-04-08 22:21:58 -0400

    Implemented JWT auth and Client Auth
    Also installed jwt-decode in client directory

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,30 +1,48 @@
-import { JwtPayload, jwtDecode } from 'jwt-decode';
+import { JwtPayload, jwtDecode } from "jwt-decode";
 
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    const token = this.getToken();
+    return token ? jwtDecode<JwtPayload>(token) : null;
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
-  
+
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    try {
+      const decoded = jwtDecode<JwtPayload & { exp: number }>(token);
+      if (decoded.exp) {
+        return decoded.exp * 1000 < Date.now();
+      }
+      return true;
+    } catch (err) {
+      return true;
+    }
   }
 
   getToken(): string {
     // TODO: return the token
+    return localStorage.getItem("id_token") || "";
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
+    localStorage.setItem("id_token", idToken);
     // TODO: redirect to the home page
+    window.location.assign("/");
   }
 
   logout() {
     // TODO: remove the token from localStorage
+    localStorage.removeItem("id_token");
     // TODO: redirect to the login page
+    window.location.assign("/login");
   }
 }
 
